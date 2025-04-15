14c6e04fd647ef3a2b90a71e37c1a56d26e9a365
Augusto Elesbão <aelesbao@users.noreply.github.com>
2025-04-15 12:54:49 +0200

    feat(nexus-cli): add link for macos arm binary

--- Formula/nexus-cli.rb
+++ Formula/nexus-cli.rb
@@ -5,7 +5,10 @@ class NexusCli < Formula
   sha256 "313d2296040cfa27e91b7b424eac9a1f1faa846b615125349577c47d23261861"
   license "Apache-2.0"
 
-  if OS.linux?
+  if Hardware::CPU.arm?
+    url "https://github.com/Talus-Network/nexus-sdk/releases/download/v#{version}/nexus-cli-#{version}-aarch64-apple-darwin.tar.gz"
+    sha256 "85af69ffeb0879ddf141fd3d339ddeb70af8fe9558188efc658429d5edbeb244"
+  elsif OS.linux?
     url "https://github.com/Talus-Network/nexus-sdk/releases/download/v#{version}/nexus-cli-#{version}-x86_64-unknown-linux-musl.tar.gz"
     sha256 "b26290ca239bc9390abd3f15d61aa1ba2452103a12fd042607841645e6b79fa4"
   end
